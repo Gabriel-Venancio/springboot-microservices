72223292e85a25f1b2f368aebccd507eaa5bda94
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-12-14 14:24:21 -0300

    setup inicial

---
+++ course/src/main/java/com/example/course/model/Course.java
@@ -0,0 +1,21 @@
+package com.example.course.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    public Long id;
+    @Column(nullable = false)
+    public String title;
+}
